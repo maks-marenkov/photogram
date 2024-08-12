45df97072c1f50cd189d124900015db759c47760
Maksim Marenkov <75558446+maks-marenkov@users.noreply.github.com>
2024-08-12 13:21:40 +0300

    red

--- src/_root/pages/EditPost.tsx
+++ src/_root/pages/EditPost.tsx
@@ -29,7 +29,7 @@ const EditPost = () => {
           <h2 className="h3-bold md:h2-bold text-left w-full">Редактировать пост</h2>
         </div>
 
-        {isLoading ? <Loader /> : <PostForm action="Update" post={post} />}
+        {isLoading ? <Loader /> : <PostForm action="Обновить" post={post} />}
       </div>
     </div>
   );
